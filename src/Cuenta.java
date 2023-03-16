c600c71b9dca4ea280e9361a46a05f6df70b6032
Jorge Escobar <jorgealejandroescobar0@gmail.com>
2023-03-15 19:14:23 -0500

    Version donde la dejé a las 7:14

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,12 +1,43 @@
 public class Cuenta {
-    protected float Saldo;
-    protected int consignaciones=0;
-    protected int retiros=0;
-    protected float tasa=0;
-    protected float comision=0;
+    public float saldo;
+    public int consignaciones=0;
+    public int retiros=0;
+    public float tasa=0;
+    public float comision=0;
 
-    public Cuenta(float saldo, float tasa) {
-        Saldo = saldo;
+    public Cuenta(float nuevoSaldo, float tasa) {
+        saldo = nuevoSaldo;
         this.tasa = tasa;
+        System.out.println("Su saldo es: "+ saldo);
+    }
+
+    public void consignar(float cantidad){
+        saldo=saldo+cantidad;
+        setSaldo(saldo);
+        consignaciones++;
+        System.out.println("Ha consignado " + cantidad + "Y ahora su saldo es " + saldo);
+        System.out.println("Cantidad total de consignaciones: " + consignaciones);
+    }
+
+    public void retirar(float cantidad){
+        if (saldo>cantidad){
+            saldo=saldo-cantidad;
+            setSaldo(saldo);
+            retiros++;
+            System.out.println("Ha retirado " + cantidad + "Y ahora su saldo es " + saldo);
+            System.out.println("Cantidad total de retiros: " + retiros);
+        }else {
+            System.out.println("Error, no tiene tanto dinero");
+        }
+
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ public class Main {
         System.out.println("Hello world!");
         Factura factura1 = new Factura(5000,"Jorge","Miguel");
         factura1.imprimirFactura();
+        Cuenta jorge= new Cuenta(20000,0.1f);
+        jorge.consignar(1500);
+
+        jorge.retirar(5000);
+        jorge.consignar(6000);
+        jorge.retirar(1000);
+        jorge.retirar(25000);
     }
 }
