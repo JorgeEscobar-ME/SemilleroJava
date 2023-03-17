b2854a83ea51dff36dd38e98f2f6097cdef4d464
Jorge Alejandro Escobar Zúñiga <jorgealejandroescobar0@gmail.com>
2023-03-16 20:08:50 -0500

    Nueva version con los getter y setter removidos porque no son atributos protegidos

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,43 +1,34 @@
 public class Cuenta {
     public float saldo;
-    public int consignaciones=0;
-    public int retiros=0;
-    public float tasa=0;
-    public float comision=0;
+    public int consignaciones = 0;
+    public int retiros = 0;
+    public float tasa = 0;
+    public float comision = 0;
 
-    public Cuenta(float nuevoSaldo, float tasa) {
-        saldo = nuevoSaldo;
+    public Cuenta(float saldo, float tasa) {
+        this.saldo = saldo;
         this.tasa = tasa;
-        System.out.println("Su saldo es: "+ saldo);
+        System.out.println("Su saldo es: " + saldo);
     }
 
-    public void consignar(float cantidad){
-        saldo=saldo+cantidad;
-        setSaldo(saldo);
+    public void consignar(float cantidad) {
+        saldo = saldo + cantidad;
         consignaciones++;
         System.out.println("Ha consignado " + cantidad + "Y ahora su saldo es " + saldo);
         System.out.println("Cantidad total de consignaciones: " + consignaciones);
     }
 
-    public void retirar(float cantidad){
-        if (saldo>cantidad){
-            saldo=saldo-cantidad;
-            setSaldo(saldo);
+    public void retirar(float cantidad) {
+        if (saldo >= cantidad) {
+            saldo = saldo - cantidad;
             retiros++;
             System.out.println("Ha retirado " + cantidad + "Y ahora su saldo es " + saldo);
             System.out.println("Cantidad total de retiros: " + retiros);
-        }else {
+        } else {
             System.out.println("Error, no tiene tanto dinero");
         }
 
     }
 
-    public float getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(float saldo) {
-        this.saldo = saldo;
-    }
 }
 
